be59e41840e7d0d2efa6fdf5d3b43581707a0e59
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2019-07-22 15:35:22 +0800

    Create build.gradle.kts

---
+++ Gradle/build.gradle.kts
@@ -0,0 +1,15 @@
+/** todo Gradle 中两个概念，Project 和 Task
+ * Project 表示一个项目，就是你想要做的某一件事
+ * Task 表示运行 Gradle 时执行的操作
+ * */
+ 
+ /** todo 创建一个 Gradle 构建任务
+  * shell> gradle -q hello    # 使用该命令运行这个任务，-q quiet 不打印 gradle 自身的 log
+  * */
+tasks.register("upper") {
+    doLast {
+        val someString = "mY_nAmE"
+        println("Original: $someString")
+        println("Upper case: ${someString.toUpperCase()}")
+    }
+}
